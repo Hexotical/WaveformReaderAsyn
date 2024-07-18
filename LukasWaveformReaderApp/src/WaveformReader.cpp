64e44958b1ffd586327383b788497692b217d2c7
Tanisha <tanisha29aggarwal@gmail.com>
2024-07-18 14:16:54 -0700

    Updated the iocsh to camelCase

--- LukasWaveformReaderApp/src/WaveformReader.cpp
+++ LukasWaveformReaderApp/src/WaveformReader.cpp
@@ -37,6 +37,7 @@ WaveformReader::WaveformReader(const char *portName, int bufferSize, int wavefor
     std::string pvIdentifier = "WAVEFORM:" + std::to_string(pvID);
     std::cout << pvIdentifier << std::endl;
     createParam(pvIdentifier.c_str(), asynParamInt16Array, &waveform_param_index);
+    std::cout << "The identifier is: " << pvIdentifier << " and the waveform_param_index is : " << waveform_param_index << std::endl;
     pv_param_map.insert(std::pair<std::string, int>(pvIdentifier, waveform_param_index));
     waveform_param_indices.push_back(pvIdentifier);
   }
@@ -70,7 +71,7 @@ void WaveformReader::statusCheck(void)
 {
   for(std::string paramIndex:waveform_param_indices)
   {
-    std::cout << paramIndex << ' ';
+    std::cout << paramIndex << ' ' << pv_param_map[paramIndex] << std::endl;
   }
 }
 
@@ -129,12 +130,12 @@ void WaveformReader::streamTask(const char *streamInit = "/Stream0", std::string
 
         std::cout << "Passed pvID: " << pvID << std::endl;
         int waveform_param_index = pv_param_map[pvID];
-        std::cout << pvID << std::endl;
+        std::cout << pvID << " corresponding index: " << waveform_param_index << std::endl;
 
         Path p;
         p = cpswGetRoot();
 
-        std::cout << "Stream Init: " << streamInit;
+        std::cout << "Stream Init (path to the stream): " << streamInit;
         Stream stm;
 
         try {
@@ -158,19 +159,25 @@ void WaveformReader::streamTask(const char *streamInit = "/Stream0", std::string
         int64_t got = 0;
         int64_t lastGot = 0;
 
+        std::cout << "Outside the while loop now " << std::endl;
+
         while(1)
         {
+            //std::cout << "INside the while loop " << std::endl;
             //std::cout << streamInit << std::endl;
-            std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+            //std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
             got = stm->read( buf, MAX_BUFFER_SIZE, CTimeout(-1));
             //std::chrono::steady_clock::time_point end1 = std::chrono::steady_clock::now();
+            //std::cout << "No. of long int values read in from the stream (value of got): " << got << std::endl;
+            //std::cout << "Value of lastGot: " << lastGot << std::endl;
 
             if(got > 8)
             {
-                //printf("Theres a thing in the stream? %ld bytes\n", got);
+                //printf("There's a thing in the stream? %ld bytes\n", got);
                 lock();
                 nBytes = (got - 9); // header = 8 bytes, footer = 1 byte, data = 32bit words.
                 nWords16 = nBytes / 2; //Amount of words in our buffer to read
+                //std::cout << "No. of words read in from the stream (after removing header and footer) : " << nWords16 << std::endl; 
 
                 doCallbacksInt16Array((epicsInt16*)(buf + 8), nWords16, waveform_param_index, 0);
 
@@ -182,7 +189,7 @@ void WaveformReader::streamTask(const char *streamInit = "/Stream0", std::string
                    */
 
                   waveformData[i] = (int16_t)buf[i];
-                  //std::cout << buf[i];
+                  //std::cout << "Value of buffer at index " << i << ": " << buf[i] << std::endl;
                 }
                 if (lastGot > got)
                 {
@@ -196,8 +203,8 @@ void WaveformReader::streamTask(const char *streamInit = "/Stream0", std::string
             {
                  asynPrint(pasynUserSelf, ASYN_TRACE_ERROR, "WaveformReader: Received frame too small\n");
             }
-            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+            //std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+            //auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
             //printf("Getting from the buffer required %llu milliseconds\n", duration);
             
         }
@@ -267,7 +274,7 @@ asynStatus WaveformReader::writeInt32(asynUser *pasynUser, epicsInt32 value)
 //IOCSH commands
 //-------------------------------------------------------------------------------------
 
-int WaveformReaderConfigure(const char* portName, int bufferSize, int waveformPVs)
+int waveformReaderConfigure(const char* portName, int bufferSize, int waveformPVs)
 {
   WaveformReader *channelManager = new WaveformReader(portName, bufferSize, waveformPVs);
   bayManager = channelManager;
@@ -278,17 +285,17 @@ static const iocshArg initArg0 = {"portName", iocshArgString};
 static const iocshArg initArg1 = {"bufferSize", iocshArgInt};
 static const iocshArg initArg2 = {"waveformPVs", iocshArgInt};
 static const iocshArg * const initArgs[] = {&initArg0, &initArg1, &initArg1};
-static const iocshFuncDef initFuncDef = {"WaveformReaderConfigure", 3, initArgs};
+static const iocshFuncDef initFuncDef = {"waveformReaderConfigure", 3, initArgs};
 static void initCallFunc(const iocshArgBuf *args)
 {
-  WaveformReaderConfigure(args[0].sval, args[1].ival, args[2].ival);
+  waveformReaderConfigure(args[0].sval, args[1].ival, args[2].ival);
 }
-void WaveformReaderRegister(void)
+void waveformReaderRegister(void)
 {
   iocshRegister(&initFuncDef, initCallFunc);
 }
 
-static void WaveformStreamInit(std::string pvID, std::string file_path) {
+static void waveformStreamInit(std::string pvID, std::string file_path) {
   bayManager->streamInit(pvID, file_path);
   return;
 }
@@ -296,60 +303,60 @@ static void WaveformStreamInit(std::string pvID, std::string file_path) {
 static const iocshArg streamArg0 = {"[channel]", iocshArgString};
 static const iocshArg streamArg1 = {"[waveform pv ID]", iocshArgString};
 static const iocshArg * const streamArgs[] = {&streamArg0, &streamArg1};
-static const iocshFuncDef optFuncDef = {"WaveformStreamInit", 2, streamArgs};
+static const iocshFuncDef optFuncDef = {"waveformStreamInit", 2, streamArgs};
 static void optCallFunc(const iocshArgBuf *args)
 {
-  WaveformStreamInit(args[1].sval, args[0].sval);
+  waveformStreamInit(args[1].sval, args[0].sval);
 }
-void WaveformStreamRegister(void)
+void waveformStreamRegister(void)
 {
   iocshRegister(&optFuncDef, optCallFunc);
 }
 
-static void WaveformStatus(void)
+static void waveformStatus(void)
 {
   bayManager->statusCheck();
   return;
 }
-static const iocshFuncDef statusFuncDef = {"WaveformStatus", 0};
+static const iocshFuncDef statusFuncDef = {"waveformStatus", 0};
 static void statusCallFunc(const iocshArgBuf *args)
 {
-  WaveformStatus();
+  waveformStatus();
 }
 
-void WaveformStatusRegister(void)
+void waveformStatusRegister(void)
 {
   iocshRegister(&statusFuncDef, statusCallFunc);
 }
 
-static void PrintHelp() 
+static void printHelp() 
 {
   std::cout << "Supported waveform commands:" << std::endl
-    << "WaveformStreamInit" << std::endl
-    << "Usage: WaveformStreamInit [channel] [waveform pv ID]" << std::endl
+    << "waveformStreamInit" << std::endl
+    << "Usage: waveformStreamInit [channel] [waveform pv ID]" << std::endl
     << "[channel] is the path to the stream that cpsw can find, eg '/Stream0' " << std::endl
     << "[waveform pv ID] refers to the asyn string identifier for a given record, generally located in OUT or INP field" <<std::endl
     << std::endl
-    << "WaveformStatus" << std::endl
-    << "Usage: WaveformStatus" << std::endl
+    << "waveformStatus" << std::endl
+    << "Usage: waveformStatus" << std::endl
     << "Health check of initialized streams." << std::endl;
 }
 
-static const iocshFuncDef helpFuncDef = {"PrintHelp", 0};
+static const iocshFuncDef helpFuncDef = {"printHelp", 0};
 static void helpCallFunc(const iocshArgBuf *args)
 {
-  PrintHelp();
+  printHelp();
 }
 
-void PrintHelpRegister(void)
+void printHelpRegister(void)
 {
   iocshRegister(&helpFuncDef, helpCallFunc);
 }
 
 extern "C" {
-  epicsExportRegistrar(PrintHelpRegister);
-  epicsExportRegistrar(WaveformStatusRegister);
-  epicsExportRegistrar(WaveformReaderRegister);
-  epicsExportRegistrar(WaveformStreamRegister);
+  epicsExportRegistrar(printHelpRegister);
+  epicsExportRegistrar(waveformStatusRegister);
+  epicsExportRegistrar(waveformReaderRegister);
+  epicsExportRegistrar(waveformStreamRegister);
 }
 
