24a71947c0ce270669251781c91955f175b272fd
Tanisha <tanisha29aggarwal@gmail.com>
2024-08-30 16:26:13 -0700

    Commit after make clean

--- iocBoot/sioc-b34-mp12/st.cmd
+++ iocBoot/sioc-b34-mp12/st.cmd
@@ -17,6 +17,7 @@ LukasWaveformReader_registerRecordDeviceDriver pdbbase
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=lujko")
 dbLoadRecords("db/waveform.db", "USER=lujko,PORT=lujko,ADDR=0,TIMEOUT=0, WAVEFORM_SIZE=500000")
+dbLoadRecords("db/waveformUnique.db", "USER=lujko,PORT=lujko,ADDR=0,TIMEOUT=0, WAVEFORM_SIZE=500000")
 
 # yamlDownloader
 DownloadYamlFile("${FPGA_IP}", "${YAML_DIR}")
